fa0809106a4a29b03cac5982893391ea55c4eca7
spenserhuang <spenserhuang515@gmail.com>
2015-06-13 18:16:57 -0700

    Initialized client side rails-api and server side rails application
    Used Rails-API and Rails to set up the decoupled architecture for the
    reflection app

---
+++ client/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Client::Application.config.secret_token = '221e6dbde9538a61a92ac741d9396899e63306c320dcc11e90b8ead3c41e639673120845964e42d012d39854780f48698ccd13771ed2cfec8f5e899d983eae79'
